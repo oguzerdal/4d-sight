dbbfdbadbbf25ec6fd7d8a06c0e9df06c9c676a4
oguzerdal <94oguzerdal@gmail.com>
2021-10-31 21:24:12 +0300

    Delete Documents/parxlab-project directory

--- Documents/parxlab-project/src/app/models/attendant.model.ts
+++
@@ -1,9 +0,0 @@
-export class Attendant {
-    constructor(
-        public id?: number,
-        public name?: string,
-        public plate?: string,
-        public attendant?: string,
-        public location?: string,
-    ) { }
-}
